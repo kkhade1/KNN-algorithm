847e047fe1663cd9c1eadb8574f4b985359e72a4
Ketaki Khade <kkhade1@CS444-kkhade1.bu.binghamton.edu>
2022-06-20 23:09:34 +0000

    knn function update1

--- submit/prj1-sol/src/knn.mjs
+++ submit/prj1-sol/src/knn.mjs
@@ -20,37 +20,55 @@ export default function  knn(testFeatures, trainLabeledFeatures, k=3){
 	// 	index: ,
 	// 	label: 
 	// }
-	for const train of trainLabeledFeatures{
+	for (var train of trainLabeledFeatures){
+		// check length of train feature and tesrFeature 
+		//console.log('train:',train);
+		//console.log('trainFeature length :',train.features.length);
+		//console.log('testfeature lenght :', testFeatures.length);
+		if (train.features.length !== testFeatures.length) {
+			return err('BAD_FMT');
+		}
 		var distance = calculate_distance(train.features, testFeatures);
+		//console.log('dist',distance);
 		distLabelIndexes.push(
 			{
 				dist : distance,
 				index : ind,
-				label : train.labels
+				label : train.label
 			}
 		);
 		ind++;
 	}
-
+	console.log('distLabelIndex : ',distLabelIndexes.slice(0,5));
 	// Sort distLabelIndexes Array by distance
 	var sorted_distLabelIndexes = [];
 	sorted_distLabelIndexes= distLabelIndexes.sort((a,b)=>a.dist-b.dist);
-
+	console.log('sorted array :',sorted_distLabelIndexes.slice(0,5));
 	//Get k smallest entries from sorted_distLabelIndexes
 	var k_smallest_entries = sorted_distLabelIndexes.slice(0,k);
 
+	console.log('K entries',k_smallest_entries);
 	//get Label and Index of maxoccurence of distance in result array
 	var result = get_maxOccurance_image(k_smallest_entries);
-	return result;
+	console.log('result: ', result);
+	return ok(result);
 }
 // trainfeatures = byte array 
 // testfeatures = byte array
 // return distance between trainfeatures and testfeatures
 function calculate_distance(trainfeatures,testfeatures){
+	const buffer = new ArrayBuffer(2);
+	const view = new DataView(buffer);
 	var sum = 0;
+	
 	for(let i=0; i< trainfeatures.length;i++){
-		sum += Math.pow((parseInt(trainfeatures[i],2) - parseInt(testfeatures[i],2)),2);
+		view.setInt8(0, trainfeatures[i]);
+		view.setInt8(1, testfeatures[i]);
+		
+		//console.log('subtract: ', view.getInt8(0) - view.getInt8(1));
+		sum += Math.pow(view.getInt8(0) - view.getInt8(1),2);
 	}
+	//console.log('sum: ', sum);
 	return sum;	
 }
 function get_maxOccurance_image(k_entries){
@@ -59,18 +77,22 @@ function get_maxOccurance_image(k_entries){
 	var maxIndex = 0;
 	var maxdist = 0;
 	var dict ={};
-	for (var e of k_entries){
-		if e.dist in dict{
-			dict[e.dist] = dict[e.dist]+1;
+	//console.log(k_entries);
+	for (let entry of k_entries){
+		console.log('dist: ', entry.dist);
+		
+		if (Object.keys(dict).includes(entry.dist)){
+			dict[entry.dist] = dict[entry.dist]+1;
 		}
 		else{
-			dict[e.dist] = 1;
+			dict[entry.dist] = 1;
 		}
-		if dict[e.dist]>maxcount{
-			maxcount = dict[e.dist];
-			maxdist = e.dist;
-			maxLabel = e.label;
-			maxIndex = e.index;
+		
+		if ((dict[entry.dist]) > maxcount){
+			maxcount = dict[entry.dist];
+			maxdist = entry.dist;
+			maxLabel = entry.label;
+			maxIndex = entry.index;
 		}
 	}
 	return [maxLabel,maxIndex];
